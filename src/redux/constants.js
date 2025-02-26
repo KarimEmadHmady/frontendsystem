aa11c59681ab31fce9f32bd38063f2089e48df39
karim emad <karimkarim20444@gmail.com>
2025-02-25 23:35:16 -0800

    edite linke

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,7 +1,7 @@
-export const BASE_URL = "";
-export const USERS_URL = "/api/users";
-export const CATEGORY_URL = "/api/category";
-export const PRODUCT_URL = "/api/products";
-export const UPLOAD_URL = "/api/upload";
-export const ORDERS_URL = "/api/orders";
-export const PAYPAL_URL = "/api/config/paypal";
+export const BASE_URL = "https://backendsystem-rnf1yw3gk-karims-projects-c3a021f0.vercel.app";
+export const USERS_URL = "https://backendsystem-rnf1yw3gk-karims-projects-c3a021f0.vercel.app/api/users";
+export const CATEGORY_URL = "https://backendsystem-rnf1yw3gk-karims-projects-c3a021f0.vercel.app/api/category";
+export const PRODUCT_URL = "https://backendsystem-rnf1yw3gk-karims-projects-c3a021f0.vercel.app/api/products";
+export const UPLOAD_URL = "https://backendsystem-rnf1yw3gk-karims-projects-c3a021f0.vercel.app/api/upload";
+export const ORDERS_URL = "https://backendsystem-rnf1yw3gk-karims-projects-c3a021f0.vercel.app/api/orders";
+export const PAYPAL_URL = "https://backendsystem-rnf1yw3gk-karims-projects-c3a021f0.vercel.app/api/config/paypal";
