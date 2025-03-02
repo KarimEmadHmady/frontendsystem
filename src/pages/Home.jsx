147385f052fa4a68ef15169b34ba77c683811ee7
karim emad <karimkarim20444@gmail.com>
2025-03-01 16:51:19 -0800

    frist commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
             الخدمة أولًا
           </p>
 
-          <h2 className="mt-6 text-1xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl">
+          <h2 className="mt-6 text-2xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl">
             احتياجات العملاء تقود تميزنا.
           </h2>
 
