7ff484e9c42fb133820aef8a84f033e341a94697
karim emad <karimkarim20444@gmail.com>
2025-03-01 16:25:02 -0800

    frist commit

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -6,6 +6,9 @@ import { useLoginMutation } from "../../redux/api/usersApiSlice";
 import { setCredentials, logout } from "../../redux/features/auth/authSlice";
 import { toast } from "react-toastify";
 import Webcam from "react-webcam";
+import { FaCamera } from "react-icons/fa";
+import { FaMapMarkerAlt } from "react-icons/fa";
+
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -89,32 +92,44 @@ const Login = () => {
 
   return (
     <div>
-      <section className="pl-[10rem] flex flex-wrap page-login-container">
-        <div className="mr-[4rem] mt-[5rem]">
-          <h1 className="text-2xl font-semibold mb-4">Sign In</h1>
+      <section className="pr-[10rem] flex flex-wrap page-login-container">
+        <div className="ml-[4rem] mt-[5rem]">
+          <h1 className="text-2xl font-semibold mb-4">تسجيل دخول</h1>
           <form onSubmit={submitHandler} className="container w-[40rem] class-page-login-reg">
             <div className="my-[2rem]">
-              <label htmlFor="email" className="block text-sm font-medium text-white">Email Address</label>
-              <input type="email" id="email" className="mt-1 p-2 border rounded w-full" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
+              <label htmlFor="email" className="block text-sm font-medium text-white">البريد الالكترونى</label>
+              <input type="email" id="email" className="mt-1 p-2 border rounded w-full" placeholder="ادخل البريد الاكترونى" value={email} onChange={(e) => setEmail(e.target.value)} />
             </div>
             <div className="mb-4">
-              <label htmlFor="password" className="block text-sm font-medium text-white">Password</label>
-              <input type="password" id="password" className="mt-1 p-2 border rounded w-full" placeholder="Enter password" value={password} onChange={(e) => setPassword(e.target.value)} />
+              <label htmlFor="password" className="block text-sm font-medium text-white">كلمة السر </label>
+              <input type="password" id="password" className="mt-1 p-2 border rounded w-full" placeholder="ادخل كلمة السر" value={password} onChange={(e) => setPassword(e.target.value)} />
             </div>
             <div className="mb-4">
               <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" className="border rounded w-full" />
-              <button type="button" onClick={capture} className="mt-2 bg-blue-500 text-white px-4 py-2 rounded">Capture Image</button>
+              <button 
+                type="button" 
+                onClick={capture} 
+                className="mt-2 flex items-center justify-center gap-2 bg-blue-500 text-white px-4 py-2 rounded shadow-md hover:bg-blue-600 transition">
+                <FaCamera className="text-lg" />
+                التقط صورة
+              </button>
               {image && <img src={image} alt="User" className="mt-2 rounded w-full" />}
             </div>
             <div className="mb-4">
-              <button type="button" onClick={getLocation} className="bg-green-500 text-white px-4 py-2 rounded">Get Location</button>
-              {location && <p className="text-white mt-2">Location: {location.latitude}, {location.longitude}</p>}
+              <button 
+                type="button" 
+                onClick={getLocation} 
+                className="bg-green-500 flex items-center justify-center gap-2 text-white px-4 py-2 rounded shadow-md hover:bg-green-600 transition">
+                <FaMapMarkerAlt className="text-lg" />
+                حدد موقعك
+              </button>
+              {location && <p className="text-white mt-2">موقعك: {location.latitude}, {location.longitude}</p>}
             </div>
-            <button disabled={isLoading} type="submit" className="bg-[#5f2476] text-white px-4 py-2 rounded cursor-pointer my-[1rem]">{isLoading ? "Signing In..." : "Sign In"}</button>
+            <button disabled={isLoading} type="submit" className="bg-[#5f2476] text-white px-4 py-2 rounded cursor-pointer my-[1rem]">{isLoading ? "تسجيل دخول..." : "تسجيل دخول"}</button>
             {isLoading && <Loader />}
           </form>
           <div className="mt-4">
-            <p className="text-white">New Customer? <Link to={redirect ? `/register?redirect=${redirect}` : "/register"} className="text-[#5f2476] hover:underline">Register</Link></p>
+            <p className="text-white">انشاء حساب جديد? <Link to={redirect ? `/register?redirect=${redirect}` : "/register"} className="text-[#5f2476] hover:underline"> تسجيل </Link></p>
           </div>
         </div>
         <img src="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fA%3D%3D&auto=format&fit=crop&w=1964&q=80" alt="" className="h-[65rem] w-[59%] xl:block md:hidden sm:hidden rounded-lg login-page-img" />
