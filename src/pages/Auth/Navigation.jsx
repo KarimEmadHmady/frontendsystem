27ce6166181a9161c1b06141548dc553fe92a83f
karim emad <karimkarim20444@gmail.com>
2025-03-01 16:57:59 -0800

    frist commit

--- src/pages/Auth/Navigation.jsx
+++ src/pages/Auth/Navigation.jsx
@@ -1,3 +1,240 @@
+// import { useState } from "react";
+// import {
+//   AiOutlineHome,
+//   AiOutlineShopping,
+//   AiOutlineLogin,
+//   AiOutlineUserAdd,
+//   AiOutlineShoppingCart,
+// } from "react-icons/ai";
+// import { FaHeart } from "react-icons/fa";
+// import { Link , useLocation } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+// import "./Navigation.css";
+// import { useSelector, useDispatch } from "react-redux";
+// import { useLogoutMutation } from "../../redux/api/usersApiSlice";
+// import { logout } from "../../redux/features/auth/authSlice";
+// import FavoritesCount from "../Products/FavoritesCount";
+
+// const Navigation = () => {
+//   const { userInfo } = useSelector((state) => state.auth);
+//   const { cartItems } = useSelector((state) => state.cart);
+//   const location = useLocation();
+
+
+
+//   const [dropdownOpen, setDropdownOpen] = useState(false);
+//   const [showSidebar, setShowSidebar] = useState(false);
+
+//   const toggleDropdown = () => {
+//     setDropdownOpen(!dropdownOpen);
+//   };
+
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+
+//   const [logoutApiCall] = useLogoutMutation();
+
+//     if (location.pathname === "/login" || location.pathname === "/register") {
+//       return null;
+//     }
+
+ 
+
+//   const logoutHandler = async () => {
+//     try {
+//       await logoutApiCall().unwrap();
+//       dispatch(logout());
+//       navigate("/login");
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+
+//   return (
+//     <div
+//       style={{ zIndex: 9999 }}
+//       className={`${
+//         showSidebar ? "hidden" : "flex"
+//       } xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-[#000] w-[4%] hover:w-[15%] h-[100vh]  fixed `}
+//       id="navigation-container"
+//     >
+//       <div className="flex flex-col justify-center  navigation-container-menu">
+//         <Link
+//           to="/"
+//           className="flex items-center transition-transform transform hover:translate-x-2"
+//         >
+//           <AiOutlineHome className="mr-2 mt-[3rem]" size={26} />
+//           <span className="hidden nav-item-name mt-[3rem]">HOME</span>{" "}
+//         </Link>
+
+//         <Link
+//           to="/shop"
+//           className="flex items-center transition-transform transform hover:translate-x-2"
+//         >
+//           <AiOutlineShopping className="mr-2 mt-[3rem]" size={26} />
+//           <span className="hidden nav-item-name mt-[3rem]">Products</span>{" "}
+//         </Link>
+
+//         <Link to="/cart" className="flex relative">
+//           <div className="flex items-center transition-transform transform hover:translate-x-2">
+//             <AiOutlineShoppingCart className="mt-[3rem] mr-2" size={26} />
+//             <span className="hidden nav-item-name mt-[3rem]">Cart</span>{" "}
+//           </div>
+
+//           <div className="absolute top-9">
+//             {cartItems.length > 0 && (
+//               <span>
+//                 <span className="px-1 py-0 text-sm text-white bg-[#5f2476] rounded-full">
+//                   {cartItems.reduce((a, c) => a + c.qty, 0)}
+//                 </span>
+//               </span>
+//             )}
+//           </div>
+//         </Link>
+
+//         <Link to="/favorite" className="flex relative">
+//           <div className="flex justify-center items-center transition-transform transform hover:translate-x-2">
+//             <FaHeart className="mt-[3rem] mr-2" size={20} />
+//             <span className="hidden nav-item-name mt-[3rem]">
+//               Favorites
+//             </span>{" "}
+//             <FavoritesCount />
+//           </div>
+//         </Link>
+//       </div>
+
+//       <div className="relative">
+//         <button 
+//           onClick={toggleDropdown}
+//           className=" btn-profile-menu  flex items-center text-gray-800 focus:outline-none"
+//         >
+//           {userInfo ? (
+//             <span className="text-white">{userInfo.username}</span>
+//           ) : (
+//             <></>
+//           )}
+//           {userInfo && (
+//             <svg
+//               xmlns="http://www.w3.org/2000/svg"
+//               className={`h-4 w-4 ml-1 ${
+//                 dropdownOpen ? "transform rotate-180" : ""
+//               }`}
+//               fill="none"
+//               viewBox="0 0 24 24"
+//               stroke="white"
+//             >
+//               <path
+//                 strokeLinecap="round"
+//                 strokeLinejoin="round"
+//                 strokeWidth="2"
+//                 d={dropdownOpen ? "M5 15l7-7 7 7" : "M19 9l-7 7-7-7"}
+//               />
+//             </svg>
+//           )}
+//         </button>
+
+//         {dropdownOpen && userInfo && (
+//           <ul
+//             className={` navi-users  absolute  -mt-[4.1rem] ml-14 space-y-2 bg-[#000] text-[#5f2476] ${
+//               !userInfo.isAdmin ? "-top-20" : "-top-80"
+//             } `}
+//           >
+//             {userInfo.isAdmin && (
+//               <>
+//                 <li>
+//                   <Link
+//                     to="/admin/dashboard"
+//                     className="block px-4 py-2 hover:bg-gray-100"
+//                   >
+//                     Dashboard
+//                   </Link>
+//                 </li>
+//                 <li>
+//                   <Link
+//                     to="/admin/productlist"
+//                     className="block px-4 py-2 hover:bg-gray-100"
+//                   >
+//                     Add Products
+//                   </Link>
+//                 </li>
+//                 <li>
+//                   <Link
+//                     to="/admin/categorylist"
+//                     className="block px-4 py-2 hover:bg-gray-100"
+//                   >
+//                     Category
+//                   </Link>
+//                 </li>
+//                 <li>
+//                   <Link
+//                     to="/admin/orderlist"
+//                     className="block px-4 py-2 hover:bg-gray-100"
+//                   >
+//                     Orders
+//                   </Link>
+//                 </li>
+//                 <li>
+//                   <Link
+//                     to="/admin/userlist"
+//                     className="block px-4 py-2 hover:bg-gray-100"
+//                   >
+//                     Users
+//                   </Link>
+//                 </li>
+//               </>
+//             )}
+
+//             <li>
+//               <Link to="/addserialNumber" className="block px-4 py-2 hover:bg-gray-100">
+//                 Add Product
+//               </Link>
+//             </li>
+//             <li>
+//               <Link to="/profile" className="block px-4 py-2 hover:bg-gray-100">
+//                 Profile
+//               </Link>
+//             </li>
+//             <li>
+//               <button
+//                 onClick={logoutHandler}
+//                 className="block w-full px-4 py-2 text-left hover:bg-gray-100"
+//               >
+//                 Logout
+//               </button>
+//             </li>
+//           </ul>
+//         )}
+//         {!userInfo && (
+//           <ul>
+//             <li>
+//               <Link
+//                 to="/login"
+//                 className="flex items-center mt-5 transition-transform transform hover:translate-x-2"
+//               >
+//                 <AiOutlineLogin className="mr-2 mt-[4px]" size={26} />
+//                 <span className="hidden nav-item-name">LOGIN</span>
+//               </Link>
+//             </li>
+//             <li>
+//               <Link
+//                 to="/register"
+//                 className="flex items-center mt-5 transition-transform transform hover:translate-x-2"
+//               >
+//                 <AiOutlineUserAdd size={26} />
+//                 <span className="hidden nav-item-name">REGISTER</span>
+//               </Link>
+//             </li>
+//           </ul>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Navigation;
+
+
 import { useState } from "react";
 import {
   AiOutlineHome,
@@ -7,7 +244,7 @@ import {
   AiOutlineShoppingCart,
 } from "react-icons/ai";
 import { FaHeart } from "react-icons/fa";
-import { Link , useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import "./Navigation.css";
 import { useSelector, useDispatch } from "react-redux";
@@ -19,8 +256,6 @@ const Navigation = () => {
   const { userInfo } = useSelector((state) => state.auth);
   const { cartItems } = useSelector((state) => state.cart);
   const location = useLocation();
-
-
 
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [showSidebar, setShowSidebar] = useState(false);
@@ -34,11 +269,9 @@ const Navigation = () => {
 
   const [logoutApiCall] = useLogoutMutation();
 
-    if (location.pathname === "/login" || location.pathname === "/register") {
-      return null;
-    }
-
- 
+  if (location.pathname === "/login" || location.pathname === "/register") {
+    return null;
+  }
 
   const logoutHandler = async () => {
     try {
@@ -50,22 +283,21 @@ const Navigation = () => {
     }
   };
 
-
   return (
     <div
       style={{ zIndex: 9999 }}
       className={`${
         showSidebar ? "hidden" : "flex"
-      } xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-[#000] w-[4%] hover:w-[15%] h-[100vh]  fixed `}
+      } xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-[#000] w-[4%] hover:w-[15%] h-[100vh] fixed`}
       id="navigation-container"
     >
-      <div className="flex flex-col justify-center  navigation-container-menu">
+      <div className="flex flex-col justify-center navigation-container-menu">
         <Link
           to="/"
           className="flex items-center transition-transform transform hover:translate-x-2"
         >
           <AiOutlineHome className="mr-2 mt-[3rem]" size={26} />
-          <span className="hidden nav-item-name mt-[3rem]">HOME</span>{" "}
+          <span className="hidden nav-item-name mt-[3rem]">الرئيسية</span>
         </Link>
 
         <Link
@@ -73,13 +305,13 @@ const Navigation = () => {
           className="flex items-center transition-transform transform hover:translate-x-2"
         >
           <AiOutlineShopping className="mr-2 mt-[3rem]" size={26} />
-          <span className="hidden nav-item-name mt-[3rem]">Products</span>{" "}
+          <span className="hidden nav-item-name mt-[3rem]">المنتجات</span>
         </Link>
 
         <Link to="/cart" className="flex relative">
           <div className="flex items-center transition-transform transform hover:translate-x-2">
             <AiOutlineShoppingCart className="mt-[3rem] mr-2" size={26} />
-            <span className="hidden nav-item-name mt-[3rem]">Cart</span>{" "}
+            <span className="hidden nav-item-name mt-[3rem]">السلة</span>
           </div>
 
           <div className="absolute top-9">
@@ -96,18 +328,16 @@ const Navigation = () => {
         <Link to="/favorite" className="flex relative">
           <div className="flex justify-center items-center transition-transform transform hover:translate-x-2">
             <FaHeart className="mt-[3rem] mr-2" size={20} />
-            <span className="hidden nav-item-name mt-[3rem]">
-              Favorites
-            </span>{" "}
+            <span className="hidden nav-item-name mt-[3rem]">المفضلة</span>
             <FavoritesCount />
           </div>
         </Link>
       </div>
 
       <div className="relative">
-        <button 
+        <button
           onClick={toggleDropdown}
-          className=" btn-profile-menu  flex items-center text-gray-800 focus:outline-none"
+          className="btn-profile-menu flex items-center text-gray-800 focus:outline-none"
         >
           {userInfo ? (
             <span className="text-white">{userInfo.username}</span>
@@ -136,9 +366,9 @@ const Navigation = () => {
 
         {dropdownOpen && userInfo && (
           <ul
-            className={` navi-users  absolute  -mt-[4.1rem] mr-14 space-y-2 bg-[#000] text-[#5f2476] ${
+            className={`navi-users absolute -mt-[4.1rem] ml-14 space-y-2 bg-[#000] text-[#5f2476] ${
               !userInfo.isAdmin ? "-top-20" : "-top-80"
-            } `}
+            }`}
           >
             {userInfo.isAdmin && (
               <>
@@ -147,7 +377,7 @@ const Navigation = () => {
                     to="/admin/dashboard"
                     className="block px-4 py-2 hover:bg-gray-100"
                   >
-                    Dashboard
+                    لوحة التحكم
                   </Link>
                 </li>
                 <li>
@@ -155,7 +385,7 @@ const Navigation = () => {
                     to="/admin/productlist"
                     className="block px-4 py-2 hover:bg-gray-100"
                   >
-                    Add Products
+                    إضافة منتج
                   </Link>
                 </li>
                 <li>
@@ -163,7 +393,7 @@ const Navigation = () => {
                     to="/admin/categorylist"
                     className="block px-4 py-2 hover:bg-gray-100"
                   >
-                    Category
+                    التصنيفات
                   </Link>
                 </li>
                 <li>
@@ -171,7 +401,7 @@ const Navigation = () => {
                     to="/admin/orderlist"
                     className="block px-4 py-2 hover:bg-gray-100"
                   >
-                    Orders
+                    الطلبات
                   </Link>
                 </li>
                 <li>
@@ -179,7 +409,7 @@ const Navigation = () => {
                     to="/admin/userlist"
                     className="block px-4 py-2 hover:bg-gray-100"
                   >
-                    Users
+                    المستخدمون
                   </Link>
                 </li>
               </>
@@ -187,12 +417,12 @@ const Navigation = () => {
 
             <li>
               <Link to="/addserialNumber" className="block px-4 py-2 hover:bg-gray-100">
-                Add Product
+                إضافة منتج
               </Link>
             </li>
             <li>
               <Link to="/profile" className="block px-4 py-2 hover:bg-gray-100">
-                Profile
+                الملف الشخصي
               </Link>
             </li>
             <li>
@@ -200,7 +430,7 @@ const Navigation = () => {
                 onClick={logoutHandler}
                 className="block w-full px-4 py-2 text-left hover:bg-gray-100"
               >
-                Logout
+                تسجيل الخروج
               </button>
             </li>
           </ul>
@@ -213,7 +443,7 @@ const Navigation = () => {
                 className="flex items-center mt-5 transition-transform transform hover:translate-x-2"
               >
                 <AiOutlineLogin className="mr-2 mt-[4px]" size={26} />
-                <span className="hidden nav-item-name">LOGIN</span>
+                <span className="hidden nav-item-name">تسجيل الدخول</span>
               </Link>
             </li>
             <li>
@@ -222,7 +452,7 @@ const Navigation = () => {
                 className="flex items-center mt-5 transition-transform transform hover:translate-x-2"
               >
                 <AiOutlineUserAdd size={26} />
-                <span className="hidden nav-item-name">REGISTER</span>
+                <span className="hidden nav-item-name">إنشاء حساب</span>
               </Link>
             </li>
           </ul>
@@ -233,3 +463,4 @@ const Navigation = () => {
 };
 
 export default Navigation;
+
