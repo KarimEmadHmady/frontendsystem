7c7c59d08cfce8c795f712c91d249d0d817d6ba3
karimemad <karimkarim20444@gmail.com>
2025-03-04 15:16:39 +0200

    4/3

--- src/components/ProgressSteps.jsx
+++ src/components/ProgressSteps.jsx
@@ -2,7 +2,7 @@ const ProgressSteps = ({ step1, step2, step3 }) => {
   return (
     <div className="flex justify-center items-center space-x-4 ">
       <div className={`${step1 ? "text-green-500" : "text-gray-300"}`}>
-        <span className="ml-2">Login</span>
+        <span className="ml-2">تسجيل دخول</span>
         <div className="mt-2 text-lg text-center">✅</div>
       </div>
 
@@ -10,7 +10,7 @@ const ProgressSteps = ({ step1, step2, step3 }) => {
         <>
           {step1 && <div className="h-0.5 w-[10rem] bg-green-500"></div>}
           <div className={`${step1 ? "text-green-500" : "text-gray-300"}`}>
-            <span>Information</span>
+            <span>معلومات الطلب</span>
             <div className="mt-2 text-lg text-center">✅</div>
           </div>
         </>
@@ -24,7 +24,7 @@ const ProgressSteps = ({ step1, step2, step3 }) => {
         )}
 
         <div className={`${step3 ? "text-green-500" : "text-gray-300"}`}>
-          <span className={`${!step3 ? "ml-[3rem]" : ""}`}>Summary</span>
+          <span className={`${!step3 ? "ml-[3rem]" : ""}`}>ملخص الطلب</span>
           {step1 && step2 && step3 ? (
             <div className="mt-2 text-lg text-center">✅</div>
           ) : (
