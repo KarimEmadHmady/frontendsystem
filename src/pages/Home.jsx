15e4be067bc715f8683a5ff36421a3a76c3a3e64
karimemad <karimkarim20444@gmail.com>
2025-03-05 14:13:19 +0200

    5-3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -136,7 +136,7 @@ const Home = () => {
       </section>
 
       <section>
-        <div className="py-8 lg:py-16 mx-auto max-w-screen-xl px-4">
+        <div className="py-8 lg:py-16 mx-auto max-w-screen-xl px-4 flex justify-center items-center flex-col">
           <h2 className="mb-8 lg:mb-16 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 dark:text-white md:text-4xl">
           ستكون في رفقة مميزة </h2>
           <img src="/imageicone.png" alt="" />
