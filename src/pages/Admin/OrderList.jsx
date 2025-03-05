009a8d86baae5a4a543159a9d5743b0c32a7d58e
karimemad <karimkarim20444@gmail.com>
2025-03-05 14:58:37 +0200

    5-3

--- src/pages/Admin/OrderList.jsx
+++ src/pages/Admin/OrderList.jsx
@@ -117,7 +117,7 @@ const OrderList = () => {
             </tbody>
           </table>
 
-          <div className="flex justify-center items-center min-h-screen">
+          <div className="flex justify-center items-center mt-[50px] ">
             <button
               onClick={deleteAllOrders}
               className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-lg shadow-md transition duration-300"
