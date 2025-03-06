addf81976e612c8419e7b775fa464c192568a8dd
karim emad <karimkarim20444@gmail.com>
2025-03-06 15:23:54 -0800

    3/6

--- src/components/ProgressSteps.jsx
+++ src/components/ProgressSteps.jsx
@@ -1,6 +1,6 @@
 const ProgressSteps = ({ step1, step2, step3 }) => {
   return (
-    <div className="flex justify-center items-center space-x-4 ">
+    <div className="flex justify-center items-center space-x-4 ml-[10px] mr-[10px] ">
       <div className={`${step1 ? "text-green-500" : "text-gray-300"}`}>
         <span className="ml-2">تسجيل دخول</span>
         <div className="mt-2 text-lg text-center">✅</div>
