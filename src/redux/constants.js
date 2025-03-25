8a65b74408c68be8c1f770fd56d97ab44684f72b
karim <karimkarim20444@gmail.com>
2025-03-25 10:43:21 -0700

    Removed .env from tracking

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://backendsystem.vercel.app';
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 // export const BASE_URL = 'http://localhost:5001';
 
 export const USERS_URL = `${BASE_URL}/api/users`;
